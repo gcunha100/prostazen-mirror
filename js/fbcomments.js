7915e0bc9281d17542164331d13c03b3e624b0af
gcunha100 <64839281+gcunha100@users.noreply.github.com>
2025-05-14 22:48:36 -0300

    Add files via upload

---
+++ js/fbcomments.js
@@ -0,0 +1,4 @@
+document.querySelector(".show_more").addEventListener("click", () => {
+	document.querySelector(".show_more").style.display = "none";
+	document.querySelector(".show_less").style.display = "inline";
+});
