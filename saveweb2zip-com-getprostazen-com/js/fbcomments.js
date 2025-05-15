f66887ee0e05be3ede12de8473553abb72312c28
gcunha100 <64839281+gcunha100@users.noreply.github.com>
2025-05-14 22:47:37 -0300

    Delete saveweb2zip-com-getprostazen-com directory

--- saveweb2zip-com-getprostazen-com/js/fbcomments.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".show_more").addEventListener("click", () => {
-	document.querySelector(".show_more").style.display = "none";
-	document.querySelector(".show_less").style.display = "inline";
-});
